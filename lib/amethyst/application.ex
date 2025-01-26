4551472d89ee0fb9524fc1f408d8bcdbd278440a
Henryk Kramer <henryk.kramer@mail.de>
2025-01-26 19:37:05 +0100

    Initialized Elixir

---
+++ lib/amethyst/application.ex
@@ -0,0 +1,20 @@
+defmodule Amethyst.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Amethyst.Worker.start_link(arg)
+      # {Amethyst.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Amethyst.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
